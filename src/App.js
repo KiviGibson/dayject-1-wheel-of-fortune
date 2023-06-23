ad62695a7215d5b567d7357270db2f77febd67e7
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2023-06-23 21:32:36 +0200

    working spinning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [items,setItems] = useState(["adam","maciek","sylvia","toster","kivi"]);
+  const [now,setNow] = useState(0)
+  const wheel = useRef(<div/>)
+  const button = useRef(<button/>)
+
+  const spin = () =>{
+    button.current.disabled = true
+    let unit = 360/items.length;
+    let force = (Math.random()*unit)*(Math.random()*3)+unit+600
+    wheel.current.style.transform = `rotate(${now+force}deg)`
+    setNow(v=>v+force)
+    setTimeout(()=>{console.log(items[parseInt(now%360/(360/items.length))]);button.current.disabled = false},6000)
+  }
   return (
     <div className="App">
-      <div className='wheel'></div>
+      <div className='wheel-side'>
+        <div className='wheel' ref={wheel}>
+          {}
+        </div>
+        <div className='arrow'/>
+      </div>
+      <button onClick={spin} ref={button}>SPIN</button>
     </div>
   );
 }
